8e3149e90cc227b9d58111b112ccdb309c970092
Lap Link Admin <admin@laplink.com>
2026-08-06 13:04:36 +0300

    Refactor shop/apps.py superuser creation using post_migrate signal to eliminate RuntimeWarning

--- shop/apps.py
+++ shop/apps.py
@@ -1,4 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def create_default_superuser(sender, **kwargs):
+    try:
+        from django.contrib.auth.models import User
+        user, created = User.objects.get_or_create(
+            username='admin',
+            defaults={'email': 'admin@example.com', 'is_staff': True, 'is_superuser': True}
+        )
+        user.set_password('AdminPass1234')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+    except Exception:
+        pass
 
 
 class ShopConfig(AppConfig):
@@ -6,15 +22,4 @@ class ShopConfig(AppConfig):
     name = 'shop'
 
     def ready(self):
-        try:
-            from django.contrib.auth.models import User
-            user, created = User.objects.get_or_create(
-                username='admin',
-                defaults={'email': 'admin@example.com', 'is_staff': True, 'is_superuser': True}
-            )
-            user.set_password('AdminPass1234')
-            user.is_staff = True
-            user.is_superuser = True
-            user.save()
-        except Exception:
-            pass
+        post_migrate.connect(create_default_superuser, sender=self)
