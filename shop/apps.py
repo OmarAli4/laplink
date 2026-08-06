cd495303a9a6fdb1dc8eceeee681271097a1d430
Lap Link Admin <admin@laplink.com>
2026-08-06 12:15:01 +0300

    Auto-create admin superuser on app startup

--- shop/apps.py
+++ shop/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class ShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
+
+    def ready(self):
+        try:
+            from django.contrib.auth.models import User
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'admin@example.com', 'AdminPass1234')
+        except Exception:
+            pass
